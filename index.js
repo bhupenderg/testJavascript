2623d1de2ca0d275b0084b8dd24d6cb8830491c3
bhupenderg <i.bhupendr.com>
2020-02-01 01:18:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let val = document.getElementsByClassName('item');
+let arr = Array.from(val);
+arr.forEach((item, index) => {
+    item.style.color = 'green';
+    item.textContent = 'Woww';
+    console.log(`${index} is ${item}`);
+})    
+console.log(val);
